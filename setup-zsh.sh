b0b9f85eee9b2d4cdada513d4cabfe86fca2c0ca
Jinfan Liu <1051666563@qq.com>
2024-10-07 16:40:09 +0800

    [Upd] Remove () in Function Call

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,18 +1,18 @@
-install_oh_my_zsh_wget(){
+install_oh_my_zsh_wget() {
   sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 }
 
-install_oh_my_zsh_curl(){
+install_oh_my_zsh_curl() {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
 # with oh-my-zsh
-install_zsh_plugins(){ 
-  # zsh-autosuggestions 
+install_zsh_plugins() {
+  # zsh-autosuggestions
   echo "Install zsh-autosuggestions"
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-  # zsh-syntax-highlighting 
+  # zsh-syntax-highlighting
   echo "Install zsh-syntax-highlighting"
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
@@ -25,12 +25,11 @@ install_zsh_plugins(){
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 }
 
-setup_zsh(){
-  install_oh_my_zsh_curl() || install_oh_my_zsh_wget() || {echo "INSTALL OH-My-ZSH with wget ERROR" && exit 1}
-
+setup_zsh() {
+  install_oh_my_zsh_curl || install_oh_my_zsh_wget || echo "INSTALL OH-My-ZSH with wget ERROR" && exit 1
   cp ./.zshrc ~/.zshrc
 
-  install_zsh_plugins() || {echo "INSTALL zsh Plugin ERROR" && exit 1}
+  install_zsh_plugins || echo "INSTALL zsh Plugin ERROR" && exit 1
 
   # Test zsh
   zsh
--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ install_basic() {
 
 install_basic || echo "INSTALL BASIC ERROR" && exit 1
 setup_binray || echo "INSTALL BINARY ERROR" && exit 1
-setup_zsh() || echo "INSTALL ZSH ERROR" && exit 1
+setup_zsh || echo "INSTALL ZSH ERROR" && exit 1
