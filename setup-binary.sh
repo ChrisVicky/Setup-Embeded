698c397c8958f232f7a03b7ef7b7988cba93fe4a
Jinfan Liu <1051666563@qq.com>
2024-10-07 16:59:34 +0800

    [Upd] Add () for wrapping

--- setup-binary.sh
+++ setup-binary.sh
@@ -11,27 +11,33 @@ install_lazygit() {
   echo "Install lazygit"
   cd ${TMP}
   [ -f ${BINDIR}/lazygit ] ||
-    wget https://github.com/jesseduffield/lazygit/releases/download/v0.44.1/lazygit_0.44.1_Linux_arm64.tar.gz &&
-    tar -xvf lazygit_0.44.1_Linux_arm64.tar.gz &&
-    mv lazygit ${BINDIR}
+    (
+      wget https://github.com/jesseduffield/lazygit/releases/download/v0.44.1/lazygit_0.44.1_Linux_arm64.tar.gz &&
+        tar -xvf lazygit_0.44.1_Linux_arm64.tar.gz &&
+        mv lazygit ${BINDIR}
+    )
 }
 
 install_lazydocker() {
   echo "Install lazydocker"
   cd ${TMP}
   [ -f ${BINDIR}/lazydocker ] ||
-    wget https://github.com/jesseduffield/lazydocker/releases/download/v0.23.3/lazydocker_0.23.3_Linux_arm64.tar.gz &&
-    tar -xvf lazydocker_0.23.3_Linux_arm64.tar.gz &&
-    mv lazydocker ${BINDIR}
+    (
+      wget https://github.com/jesseduffield/lazydocker/releases/download/v0.23.3/lazydocker_0.23.3_Linux_arm64.tar.gz &&
+        tar -xvf lazydocker_0.23.3_Linux_arm64.tar.gz &&
+        mv lazydocker ${BINDIR}
+    )
 }
 
 install_zellij() {
   echo "Install zellij"
   cd ${TMP}
   [ -f ${BINDIR}/zellij ] ||
-    wget https://github.com/zellij-org/zellij/releases/download/v0.40.1/zellij-aarch64-unknown-linux-musl.tar.gz &&
-    tar -xvf zellij-aarch64-unknown-linux-musl.tar.gz &&
-    mv zellij ${BINDIR}
+    (
+      wget https://github.com/zellij-org/zellij/releases/download/v0.40.1/zellij-aarch64-unknown-linux-musl.tar.gz &&
+        tar -xvf zellij-aarch64-unknown-linux-musl.tar.gz &&
+        mv zellij ${BINDIR}
+    )
 }
 
 post_setup_binary() {
